2b1a193876e29bd9fdd0082bff6713a9ab507b67
soata <heytoway@gmail.com>
2019-01-11 16:23:21 +0900

    update:   + mute when no items found   + change keybinding descriptions   + add some variables on alfredworkflow

---
+++ README.md
@@ -0,0 +1,72 @@
+# go-ghq-alfred
+
+[![Build Status](https://travis-ci.org/pddg/go-ghq-alfred.svg?branch=master)](https://travis-ci.org/pddg/go-ghq-alfred)
+
+Search local repos with ghq in Alfred Workflow.
+
+## Environment
+
+* Alfred 3.4 (or later)
+* [ghq](https://github.com/motemen/ghq)
+
+## Usage
+
+This tool is a CLI tool. Output JSON strings.
+
+```bash
+$ ./go-alfred-ghq '{query}' $(ghq list -p)
+```
+
+## In Alfred
+
+This workflow start with `ghq {query}` in alfred.  
+
+Filtering the result of `ghq list -p` with `{query}` and show them.
+
+### Preparing
+
+You should specify path to `ghq`. Open this workflow settings, and edit environment variables. Default is `/usr/local/bin/ghq`.  
+
+And I recommend you to specify a editor and terminal app. Default is `Visual Studio Code.app` and `iTerm.app`
+
+### Modifier key options
+
+* **Enter**: Open repository in Finder.
+* **Shift + Enter**: Open repository in your default browser.
+* **Command + Enter**: Same as Enter only.
+* **Option + Enter**: Search "user/repo" in google.
+* **Fn + Enter**: Open repository in your terminal.
+* **Control + Enter**: Open repository in your editor.
+
+## Build
+
+My environment is as follows.
+
+* Go 1.9
+* Glide 0.12.3
+
+```bash
+$ glide update
+$ go test $(glide novendor) -v
+$ go build .
+```
+
+## Attributes
+
+Icons provided by www.flaticon.com.
+
+### github and git logo
+
+Icon made by Freepik from www.flaticon.com
+
+### bitbucket logo
+
+Icon made by Swifticons from www.flaticon.com
+
+## Author
+
+pudding
+
+## License
+
+MIT
---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	model "github.com/pddg/alfred-models"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "ghq-alfred"
+	app.Usage = "Search your local repos"
+	app.Version = "0.4.0"
+	app.Action = func(c *cli.Context) error {
+		resp := model.NewResponse()
+		query := strings.Trim(c.Args()[0], " \n")
+		repos := c.Args()[1:c.NArg()]
+		ch := make(chan *model.Item)
+		wg := &sync.WaitGroup{}
+		for index, repo := range repos {
+			wg.Add(1)
+			go worker(index, repo, query, wg, ch)
+		}
+		go waitForWorker(wg, ch)
+		for item := range ch {
+			resp.Items = append(resp.Items, *item)
+		}
+		if resp.Items == nil {
+			// When any item is not found.
+			// item := createNoResultItem()
+			// resp.Items = append(resp.Items, *item)
+		}
+		j, err := json.Marshal(resp)
+		if err != nil {
+			// Json error
+			fmt.Println("{'items': [{'title': 'Json object is invalid.', 'subtitle': 'Please contact to developper.', 'valid': false}]")
+		} else {
+			fmt.Println(string(j))
+		}
+		return nil
+	}
+	app.Run(os.Args)
+}
+
+func waitForWorker(wg *sync.WaitGroup, ch chan *model.Item) {
+	wg.Wait()
+	close(ch)
+}
+
+func worker(index int, repo string, query string, wg *sync.WaitGroup, ch chan *model.Item) {
+	defer wg.Done()
+	path := strings.Split(repo, "/")
+	if matchRepo(path, query) {
+		// Create normal item
+		item := createNewItem(index, repo, path)
+		ch <- item
+	}
+}
+
+func createNewItem(index int, repo string, repo_path []string) *model.Item {
+	item := model.NewItem()
+	item.Uid = string(index)
+	item.Title = excludeDomain(repo_path, true)
+	item.Subtitle = getDomainName(repo_path)
+	item.Arg = repo
+	item.Icon.Type = ""
+	item.Icon.Path = getIconPath(repo_path)
+	createModItems(repo_path, repo, &item.Mods)
+	return item
+}
+
+func createNoResultItem() *model.Item {
+	item := model.NewItem()
+	item.Title = "No result found."
+	item.Subtitle = "Please input again."
+	item.Valid = false
+	return item
+}
+
+func matchRepo(repo_path []string, query string) bool {
+	repo_path_lower, query_lower := strings.ToLower(excludeDomain(repo_path, true)), strings.ToLower(query)
+	if strings.Index(repo_path_lower, query_lower) != -1 {
+		return true
+	}
+	return false
+}
+
+func excludeDomain(repo []string, domain bool) string {
+	// full_repo_path: strings.Split("/path/to/github.com/user/full_repo_path", "/")
+	var i int
+	if domain {
+		// return user/full_repo_path
+		i = 2
+	} else {
+		// return github.com/user/full_repo_path
+		i = 3
+	}
+	length := len(repo)
+	return strings.Join(repo[length-i:length], "/")
+}
+
+func getDomainName(repo_path []string) string {
+	// return github.com
+	return repo_path[len(repo_path)-3]
+}
+
+func createModItems(repo []string, path string, mods *map[string]model.Mod) {
+	for _, key := range model.Modifiers {
+		var (
+			arg string
+			sub string
+		)
+		switch key {
+		case model.Cmd:
+			arg = path
+			sub = "Open '" + path + "' in editor."
+		case model.Shift:
+			arg = "https://" + excludeDomain(repo, false) + "/"
+			sub = "Open '" + arg + "' in browser."
+		case model.Ctrl:
+			arg = path
+			sub = "Open '" + path + "' in terminal."
+		case model.Alt:
+			arg = excludeDomain(repo, true)
+			sub = "Search '" + arg + "' with google."
+		}
+		mod := model.NewMod(arg, sub)
+		(*mods)[key] = *mod
+	}
+}
+
+func getIconPath(repo_path []string) string {
+	var icon_path string
+	domain := getDomainName(repo_path)
+	prefix := "./resources"
+	switch {
+	case strings.Contains(domain, "github"):
+		icon_path = prefix + "/github-logo.png"
+	case strings.Contains(domain, "bitbucket"):
+		icon_path = prefix + "/bitbucket-logo.png"
+	case strings.Contains(domain, "gitlab"):
+		icon_path = prefix + "/gitlab-logo.png"
+	default:
+		icon_path = prefix + "/git-logo.png"
+	}
+	return icon_path
+}
